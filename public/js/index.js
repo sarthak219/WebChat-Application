5b608386b9084cb8e0236e1a30eeb112e6d22e13
sarthakchauhan <schauh04>
2021-07-17 11:53:02 +0530

    highlighted active contact in menu panel

--- public/js/index.js
+++ public/js/index.js
@@ -8,13 +8,12 @@ $(document).ready(() => {
     const senderName = document.querySelector("#senderName");
     const receiverName = document.querySelectorAll("#receiverName");
     const contactName = document.querySelectorAll(".contactName");
+
     receiverTitle.innerText = capitalizeFirstLetter(receiverTitle.innerText);
     senderName.innerText = capitalizeFirstLetter(senderName.innerText);
-
     receiverName.forEach((name) => {
         name.innerText = capitalizeFirstLetter(name.innerText);
     });
-
     contactName.forEach((name) => {
         name.innerText = capitalizeFirstLetter(name.innerText);
     });
@@ -32,3 +31,29 @@ $(document).ready(() => {
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+// mouse positions
+
+// function mouseX(evt) {
+//     if (evt.pageX) {
+//         return evt.pageX;
+//     } else if (evt.clientX) {
+//         return evt.clientX + (document.documentElement.scrollLeft ?
+//             document.documentElement.scrollLeft :
+//             document.body.scrollLeft);
+//     } else {
+//         return null;
+//     }
+// }
+
+// function mouseY(evt) {
+//     if (evt.pageY) {
+//         return evt.pageY;
+//     } else if (evt.clientY) {
+//         return evt.clientY + (document.documentElement.scrollTop ?
+//             document.documentElement.scrollTop :
+//             document.body.scrollTop);
+//     } else {
+//         return null;
+//     }
+// }
